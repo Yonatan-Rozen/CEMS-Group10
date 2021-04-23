88a985a7fae8765b62a4d5c6287b7db3055d0bec
Jon <yon969@outlook.co.il>
2021-04-23 12:41:41 +0300

    Added new java class - DBconnector.java

---
+++ G10_CEMS/src/control/DBconnector.java
@@ -0,0 +1,32 @@
+package control;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnector 
+{
+	static Connection con;
+	public static void main(String[] args) 
+	{
+		try 
+		{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver definition succeed");
+        } catch (Exception ex) {
+        	/* handle the error*/
+        	System.out.println("Driver definition failed");
+    	}
+        
+        try 
+        {
+            con = DriverManager.getConnection("jdbc:mysql://localhost/CEMS?serverTimezone=IST","root","Group10*");
+            System.out.println("SQL connection succeed");
+     	} catch (SQLException ex) {
+     		/* handle any errors*/
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+   	}
+}
