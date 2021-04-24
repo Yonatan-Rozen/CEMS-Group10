5af3f0a3f74b5da68d2451f69774ff23f4686faa
Jon <yon969@outlook.co.il>
2021-04-24 22:18:30 +0300

    Added requirements for the 'gui' package

---
+++ G10_CEMS/src/gui/GUI.java
@@ -0,0 +1,8 @@
+package gui;
+
+public class GUI 
+{
+	public static void main(String[] args) {
+		System.out.println("gui package should contain FXML files (SceneBuilder) + controllers + css files");
+	}
+}
