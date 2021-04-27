35ab96ac9380e83c109ffd545451c7a8b70932c5
Jon <yon969@outlook.co.il>
2021-04-28 00:51:47 +0300

    Changed all variables to SimpleProperties values

--- G10_CEMS/src/logic/Exam.java
+++ G10_CEMS/src/logic/Exam.java
@@ -1,15 +1,17 @@
 package logic;
 
-import java.util.ArrayList;
-import java.util.List;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 
 public class Exam {
 
-	private String eID;
-	private String eProfession;
-	private String eCourse;
-	private int eAllocatedTime;
-	private List<Integer> scores = new ArrayList<>();
+	private SimpleStringProperty eID;
+	private SimpleStringProperty eProfession;
+	private SimpleStringProperty eCourse;
+	private SimpleIntegerProperty eAllocatedTime;
+	private SimpleStringProperty eScores;
+	//private SimpleListProperty<Integer> hi = new SimpleListProperty<>();
+	//private List<Integer> scores = new ArrayList<>();
 	
 	/**
 	 * @param eID 		 	 exam ID [format : ##$$%%]  [%% - represents exam number]
@@ -18,15 +20,18 @@ public class Exam {
 	 * @param eAllocatedTime allocated time (in minutes)
 	 * @param scores		 array of scores. the length represents the amount of questions
 	 */
-	public Exam(String eID, String eProfession, String eCourse, int eAllocatedTime, String scores)
+	public Exam(String eID, String eProfession, String eCourse, int eAllocatedTime, String eScores)
 	{
-		setEID(eID);
-		setEProfession(eProfession);
-		setECourse(eCourse);
-		setEAllocatedTime(eAllocatedTime);
-		String[] sArr = scores.split("\\|");
-		for (String s: sArr)
-			this.scores.add(Integer.parseInt(s));
+		this.eID = new SimpleStringProperty(eID);
+		this.eProfession = new SimpleStringProperty(eProfession);
+		this.eCourse = new SimpleStringProperty(eCourse);
+		this.eAllocatedTime = new SimpleIntegerProperty(eAllocatedTime);
+		this.eScores = new SimpleStringProperty(eScores);
+		
+		
+		//String[] sArr = scores.split("\\|");
+		//for (String s: sArr)
+			//this.scores.add(Integer.parseInt(s));
 	}
 	
 	// setters & getters **********************************************************
@@ -35,67 +40,71 @@ public class Exam {
 	 * @return the exam ID
 	 */
 	public String getEID() {
-		return eID;
+		return eID.get();
 	}
 	/**
 	 * @param eID - set exam ID
 	 */
 	public void setEID(String eID) {
-		this.eID = eID;
+		this.eID.set(eID);
 	}
 	
 	/**
 	 * @return the exam profession name
 	 */
 	public String getEProfession() {
-		return eProfession;
+		return eProfession.get();
 	}
 	/**
 	 * @param eProfession - set exam profession
 	 */
 	public void setEProfession(String eProfession) {
-		this.eProfession = eProfession;
+		this.eProfession.set(eProfession);
 	}
 	
 	/**
 	 * @return the exam course name
 	 */
 	public String getECourse() {
-		return eCourse;
+		return eCourse.get();
 	}
 	/**
 	 * @param courseName - set name of exam course
 	 */
 	public void setECourse(String courseName) {
-		this.eCourse = courseName;
+		this.eCourse.set(courseName);
 	}
 	
 	/**
 	 * @return the exam allocated Time
 	 */
 	public int getEAllocatedTime() {
-		return eAllocatedTime;
+		return eAllocatedTime.get();
 	}
 	
 	/**
 	 * @param eAllocatedTime - set exam allocated time (in minutes)
 	 */
 	public void setEAllocatedTime(int eAllocatedTime) {
-		this.eAllocatedTime = eAllocatedTime;
+		this.eAllocatedTime.set(eAllocatedTime);
+	}
+
+	public String getEScores() {
+		return eScores.get();
+	}
+
+	public void setEScores(String eScores) {
+		this.eScores.set(eScores);
 	}
 	
-	// toString *******************************************************************
 	
+	// toString *******************************************************************
+
 	@Override
 	public String toString() {
-		String ret = String.format("ID: %s%nProfession: %s%nCourse: %s%n"
-				+ "Allocated Time: %d%nThere are %d questions. "
-				+ "These are their scores (in order):%n", eID, eProfession, eCourse,
-				eAllocatedTime, scores.size());
-		int index = 1;
-		for (Integer sc : scores)
-			ret += String.format("%d) %d%n", index++, sc);
-		return ret;
+		return String.format("ID: %s%nProfession: %s%nCourse: %s%n"
+				+ "Allocated Time: %d%nScores (in order): {%s}", 
+				getEID(), getEProfession(), getECourse(), getEAllocatedTime(), getEScores());
 	}
 	// End of Exam ****************************************************************
 	
