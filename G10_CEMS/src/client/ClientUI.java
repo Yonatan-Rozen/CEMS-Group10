885eaa1ffb39a2e8b0531f88c7c62034c5ab99a1
Jon <yon969@outlook.co.il>
2021-05-03 00:32:51 +0300

    Removed unnecessary Import

--- G10_CEMS/src/client/ClientUI.java
+++ G10_CEMS/src/client/ClientUI.java
@@ -3,7 +3,6 @@ package client;
 import gui.ClientMenuController;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import server.ServerUI;
 
 public class ClientUI extends Application {
 	public static ClientController chat; // only one instance
@@ -14,7 +13,7 @@ public class ClientUI extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		chat = new ClientController("localhost", 5555);
+		chat = new ClientController("localhost", 5555); 
 		chat.accept("client connected successfully");
 		ClientMenuController cmc = new ClientMenuController();
 		cmc.start(primaryStage);
