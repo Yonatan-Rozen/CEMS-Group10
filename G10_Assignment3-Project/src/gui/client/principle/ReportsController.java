43418bf048279ffdda04858f11bb6ced126ae165
Jon <yon969@outlook.co.il>
2021-05-15 17:56:20 +0300

    Added Reports files (fxml + Controller)

---
+++ G10_Assignment3-Project/src/gui/client/principle/ReportsController.java
@@ -0,0 +1,88 @@
+package gui.client.principle;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import client.ClientUI;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ReportsController implements Initializable {
+
+	@FXML
+	private BarChart<?, ?> sbHistogramBc;
+
+	@FXML
+	private CategoryAxis sbCategoryAxis;
+
+	@FXML
+	private NumberAxis sbNumberAxis;
+
+	@FXML
+	private Label sbReportsByLbl;
+
+	@FXML
+	private Label sbExamIDLbl;
+
+	@FXML
+	private Label sbMedianLbl;
+
+	@FXML
+	private Label sbAverageLbl;
+
+	@FXML
+	private Button sbNextRepBtn;
+
+	@FXML
+	private Button sbPreviousRepBtn;
+
+	private BarChart<?, ?> histogramBc;
+	private CategoryAxis categoryAxis;
+	private NumberAxis numberAxis;
+	private Label reportsByLbl;
+	private Label examIDLbl;
+	private Label medianLbl;
+	private Label averageLbl;
+	private Button nextRepBtn;
+	private Button previousRepBtn;
+	
+
+	public void start() throws IOException {
+		// TODO Auto-generated method stub
+		//example **************************************
+		PrincipleMenuBarController.mainPaneBp.setCenter(FXMLLoader.load(getClass().getResource("/gui/client/principle/Reports.fxml")));
+		//**********************************************
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		histogramBc = sbHistogramBc;
+		categoryAxis = sbCategoryAxis;
+		numberAxis = sbNumberAxis;
+		reportsByLbl = sbReportsByLbl;
+		examIDLbl = sbExamIDLbl;
+		medianLbl = sbMedianLbl;
+		averageLbl = sbAverageLbl;
+		nextRepBtn = sbNextRepBtn;
+		previousRepBtn = sbPreviousRepBtn;
+	}
+
+	@FXML
+	void BtnPressPrevousRep(ActionEvent event) {
+		// TODO show the next report
+	}
+
+	@FXML
+	void btnPressNextRep(ActionEvent event) {
+		// TODO show the previous report
+	}
+
+}
