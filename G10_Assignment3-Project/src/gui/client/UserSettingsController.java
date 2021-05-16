92945610d4cc74aab390e7c740f84ba6faaf74de
Jon <yon969@outlook.co.il>
2021-05-16 22:40:18 +0300

    Added UserSettings files (fxml + controller)

---
+++ G10_Assignment3-Project/src/gui/client/UserSettingsController.java
@@ -0,0 +1,58 @@
+package gui.client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+
+public class UserSettingsController implements Initializable {
+
+	// JAVAFX INSTANCES ******************************************************
+	@FXML
+	private Hyperlink sbChangePassLnk;
+
+	@FXML
+	private Label sbUsernameLbl;
+
+	@FXML
+	private Label sbFullnameLbl;
+
+	@FXML
+	private Label sbPhoneLbl;
+
+	@FXML
+	private Label sbEmailLbl;
+
+	@FXML
+	private Label sbAccountTypeLbl;
+	
+	// STATIC JAVAFX INSTANCES **********************************************
+	private static Hyperlink changePassLnk;
+	private static Label usernameLbl;
+	private static Label fullnameLbl;
+	private static Label phoneLbl;
+	private static Label emailLbl;
+	private static Label accountTypeLbl;
+	
+	// INITIALIZE METHOD ****************************************************
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		changePassLnk = sbChangePassLnk;
+		usernameLbl = sbUsernameLbl;
+		fullnameLbl = sbFullnameLbl;
+		phoneLbl = sbPhoneLbl;
+		emailLbl = sbEmailLbl;
+		accountTypeLbl = sbAccountTypeLbl;
+	}
+
+	// ACTION METHODS *******************************************************
+	@FXML
+	void lnkPressChangePass(ActionEvent event) {
+
+	}
+
+}
