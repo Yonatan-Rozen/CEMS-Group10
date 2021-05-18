24d06311a8cf0fa86ed3f885fb1643f8ad56e276
Jon <yon969@outlook.co.il>
2021-05-18 21:15:12 +0300

    Changed an import from java.awt.* to javafx.*

--- G10_Assignment3-Project/src/gui/client/teacher/TeacherStartExamController.java
+++ G10_Assignment3-Project/src/gui/client/teacher/TeacherStartExamController.java
@@ -1,74 +1,56 @@
 package gui.client.teacher;
 
-import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class TeacherStartExamController {
+public class TeacherStartExamController implements Initializable{
 
-    @FXML
-    private Button sbBackBtn;
+	@FXML
+	private Button sbBackBtn;
 
-    @FXML
-    private ComboBox<?> sbChooseExamComboBox;
+	@FXML
+	private ComboBox<?> sbChoseExamCb;
 
-    @FXML
-    private TextField Text4Digits;
+	@FXML
+	private TextField sbCodeTf;
 
-    @FXML
-    private Button ButtonConfirm;
+	@FXML
+	private Button sbStartBtn;
+	
+	private Button backBtn;
+	private ComboBox<?> choseExamCb;
+	private TextField codeTf;
+	private Button startBtn;
 
-    @FXML
-    void ComboBoxChooseExam(ActionEvent event) {
-
-    }
-
-    @FXML
-    void PressConfirm(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnBack(ActionEvent event) {
-
-    }
-
-
-    
-    private static Stage currentStage;
-    
-    public void start(Stage mainStage) {
-		try {
-			currentStage = mainStage;
-			Parent root = FXMLLoader.load(getClass().getResource("/gui/teacher/TeacherStartExam.fxml"));
-			Scene scene = new Scene(root);
-			// scene.getStylesheets().add(getClass().getResource("/gui/StudentSettings.css").toExternalForm());
-			mainStage.setTitle("Start Exam");
-			mainStage.setScene(scene);
-			mainStage.setResizable(false);
-			mainStage.setOnCloseRequest(e -> {
-				// ClientUI.chat.accept("client disconnected");
-				mainStage.hide();
-				Alert alert = new Alert(AlertType.INFORMATION);
-				alert.setTitle("Client disconnected");
-				alert.setHeaderText("You have been disconnected from the server(StudentSettings)");
-				alert.setContentText("Press ok to continue...");
-				alert.showAndWait();
-				System.exit(0);
-			});
-			mainStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage mainStage) throws IOException {
 	}
 
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		backBtn=sbBackBtn;
+		choseExamCb=sbChoseExamCb;
+		codeTf=sbCodeTf;
+		startBtn=sbStartBtn;
+	}
+
+	@FXML
+	void btnPressBack(ActionEvent event) {
+	}
+
+	@FXML
+	void btnPressStart(ActionEvent event) {
+	}
+
+	@FXML
+	void cbPressChoseExam(ActionEvent event) {
+	}
 }
