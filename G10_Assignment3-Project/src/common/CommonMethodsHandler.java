b511d790c9e4952bd18b43fc3c0f049a435bf4de
Jon <yon969@outlook.co.il>
2021-05-18 21:11:56 +0300

    Changed to StringBuilder, and added a print to the path of the fxml file

--- G10_Assignment3-Project/src/common/CommonMethodsHandler.java
+++ G10_Assignment3-Project/src/common/CommonMethodsHandler.java
@@ -27,21 +27,26 @@ public class CommonMethodsHandler {
 	 */
 	public Pane getPane(String packageName, String fxmlName) {
 		Pane pane = null;
+		StringBuilder path = new StringBuilder("gui/");
 		URL fileUrl = null;
 		try {
 			switch (packageName) {
 				case "student":
 				case "principle":
 				case "teacher":
-					fileUrl = getClass().getResource("/gui/client/" + packageName + "/" + fxmlName + ".fxml");
+					path.append("client/").append(packageName).append("/").append(fxmlName).append(".fxml");
+					//fileUrl = getClass().getResource("/gui/client/" + packageName + "/" + fxmlName + ".fxml");
 					break;
 				case "client":
 				case "server":
-					fileUrl = getClass().getResource("/gui/"+ packageName +"/" + fxmlName + ".fxml");
+					path.append(packageName).append("/").append(fxmlName).append(".fxml");
+					//fileUrl = getClass().getResource("/gui/"+ packageName +"/" + fxmlName + ".fxml");
 					break;
 				default:
 					break;
 			}
+			System.out.println(path.toString());
+			fileUrl = getClass().getClassLoader().getResource(path.toString());
 			if (fileUrl == null) {
 				throw new FileNotFoundException(fxmlName + ".fxml could not be found!");
 			}
