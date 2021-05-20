c900c3fa448b7be1d6dc3c2ec9b07afe29f0f126
Jon <yon969@outlook.co.il>
2021-05-21 01:34:16 +0300

    Fixed window size issues

--- G10_Assignment3-Project/src/gui/client/student/StudentExamSubmittedController.java
+++ G10_Assignment3-Project/src/gui/client/student/StudentExamSubmittedController.java
@@ -1,14 +1,17 @@
 package gui.client.student;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import client.ClientUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class StudentExamSubmittedController {
+public class StudentExamSubmittedController implements Initializable{
 
 	@FXML
 	private Button sbBackToMenuBtn;
@@ -16,6 +19,12 @@ public class StudentExamSubmittedController {
 	@FXML
 	void btnPressBackToMenu(ActionEvent event) throws IOException {
 		ClientUI.mainScene.setRoot(FXMLLoader.load(getClass().getResource("/gui/client/student/StudentMenu.fxml")));
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		ClientUI.mainStage.setWidth(750);
+		ClientUI.mainStage.setHeight(400);
 	}
 
 }
