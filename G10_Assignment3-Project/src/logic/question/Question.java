4452c879af9caa2f002d352c97f2c7dc5997df7f
Jon <yon969@outlook.co.il>
2021-05-30 03:11:56 +0300

    Added parameters, constructor & getters&setters methods

--- G10_Assignment3-Project/src/logic/question/Question.java
+++ G10_Assignment3-Project/src/logic/question/Question.java
@@ -1,5 +1,67 @@
 package logic.question;
 
-public class Question {
+import java.io.Serializable;
 
+public class Question implements Serializable {
+	
+	private String questionID;
+	private String questionBody;
+	private String answer1;
+	private String answer2;
+	private String answer3;
+	private String answer4;
+	private String correctAnswer;
+
+	public Question(String questionID, String questionBody, String answer1, String answer2, String answer3, String answer4, String correctAnswer) {
+		this.questionID = questionID;
+		this.questionBody = questionBody;
+		this.answer1 = answer1;
+		this.answer2 = answer2;
+		this.answer3 = answer3;
+		this.answer4 = answer4;
+		this.correctAnswer = correctAnswer;
+	}
+
+	public String getQuestionID() {
+		return questionID;
+	}
+	public void setQuestionID(String questionID) {
+		this.questionID = questionID;
+	}
+	public String getQuestionBody() {
+		return questionBody;
+	}
+	public void setQuestionBody(String questionBody) {
+		this.questionBody = questionBody;
+	}
+	public String getAnswer1() {
+		return answer1;
+	}
+	public void setAnswer1(String answer1) {
+		this.answer1 = answer1;
+	}
+	public String getAnswer2() {
+		return answer2;
+	}
+	public void setAnswer2(String answer2) {
+		this.answer2 = answer2;
+	}
+	public String getAnswer3() {
+		return answer3;
+	}
+	public void setAnswer3(String answer3) {
+		this.answer3 = answer3;
+	}
+	public String getAnswer4() {
+		return answer4;
+	}
+	public void setAnswer4(String answer4) {
+		this.answer4 = answer4;
+	}
+	public String getCorrectAnswer() {
+		return correctAnswer;
+	}
+	public void setCorrectAnswer(String correctAnswer) {
+		this.correctAnswer = correctAnswer;
+	}
 }
