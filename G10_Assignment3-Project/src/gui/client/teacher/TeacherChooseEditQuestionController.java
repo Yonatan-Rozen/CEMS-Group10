141575860132defc28859fd1e32de7f01ed87d5e
Jon <yon969@outlook.co.il>
2021-06-01 02:51:50 +0300

    Orginized 'TeacherChooseEditQuestion' files (fxml + controller)

--- G10_Assignment3-Project/src/gui/client/teacher/TeacherChooseEditQuestionController.java
+++ G10_Assignment3-Project/src/gui/client/teacher/TeacherChooseEditQuestionController.java
@@ -7,19 +7,25 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
-public class TeacherEditQuestionController implements Initializable {
+public class TeacherChooseEditQuestionController implements Initializable {
 
 	// JAVAFX INSTNCES ******************************************************
 	@FXML
-	private AnchorPane sbBotPanelAp;
+	private AnchorPane sbTopPanelAp;
+	
+	@FXML
+	private ChoiceBox<String> sbQuestionSubject;
 
 	@FXML
-	private Button sbEditQuestionBtn;
+	private Button sbShowQuestionsBySubjectBtn;
+	
+	@FXML
+	private AnchorPane sbBotPanelAp;
 
 	@FXML
 	private TableView<String> sbAvailableQuestionsTv;
@@ -28,80 +34,51 @@ public class TeacherEditQuestionController implements Initializable {
 	private TableColumn<?, String> sbQuestionIDTc;
 
 	@FXML
-	private Button sbChangeBankBtn;
+	private Button sbChangeSubjectBtn;
 
 	@FXML
-	private Button sbPreviewQuestionBtn;
-
-	@FXML
-	private AnchorPane sbTopPanelAp;
-
-	@FXML
-	private ComboBox<String> sbChooseBankCb;
-
-	@FXML
-	private Button sbShowQuestionsByBankBtn;
-
-	@FXML
-	private Button sbDiscardChangesBtn;
+	private Button sbEditQuestionBtn;
 
 	// STATIC JAVAFX INSTANCES **********************************************
+	private static AnchorPane topPanelAp;
+	private static ChoiceBox<String> questionSubject;
+	private static Button showQuestionsBySubjectBtn;
 	private static AnchorPane botPanelAp;
-	private static Button editQuestionBtn;
 	private static TableView<String> availableQuestionsTv;
 	private static TableColumn<?, String> questionIDTc;
-	private static Button changeBankBtn;
-	private static Button previewQuestionBtn;
-	private static AnchorPane topPanelAp;
-	private static ComboBox<String> chooseBankCb;
-	private static Button showQuestionsByBankBtn;
-	private static Button discardChangesBtn;
+	private static Button changeSubjectBtn;
+	private static Button editQuestionBtn;
 
 	// INITIALIZE METHOD ****************************************************
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		/**** First panel ****/
 		topPanelAp = sbTopPanelAp;
-		chooseBankCb = sbChooseBankCb;
-		discardChangesBtn = sbDiscardChangesBtn;
-		showQuestionsByBankBtn = sbShowQuestionsByBankBtn;
+		questionSubject = sbQuestionSubject;
+		showQuestionsBySubjectBtn = sbShowQuestionsBySubjectBtn;
+		
+		/**** Second panel ****/
 		botPanelAp = sbBotPanelAp;
 		botPanelAp.setDisable(true);
-		editQuestionBtn = sbEditQuestionBtn;
 		availableQuestionsTv = sbAvailableQuestionsTv;
 		questionIDTc = sbQuestionIDTc;
-		changeBankBtn = sbChangeBankBtn;
-		previewQuestionBtn = sbPreviewQuestionBtn;
+		changeSubjectBtn = sbChangeSubjectBtn;
+		editQuestionBtn = sbEditQuestionBtn;
 	}
 
 	// ACTION METHODS *******************************************************
 	@FXML
-	void cbPressChooseBank(ActionEvent event) {
-		System.out.println("TeacherEditQuestion::cbPressChooseBank");
-	}
-
-	@FXML
-	void btnPressDiscardChanges(ActionEvent event) {
-		System.out.println("TeacherEditQuestion::btnPressDiscardChanges");
-	}
-
-	@FXML
-	void btnPressShowQuestionsByBank(ActionEvent event) {
+	void btnPressShowQuestionsBySubject(ActionEvent event) {
 		System.out.println("TeacherEditQuestion::btnPressShowQuestionsByBank");
 		topPanelAp.setDisable(true);
 		botPanelAp.setDisable(false);
 	}
 
 	@FXML
-	void btnPressPreviewQuestion(ActionEvent event) {
-		System.out.println("TeacherEditQuestion::btnPressPreviewQuestion");
-	}
-
-	@FXML
-	void btnPressChangeBank(ActionEvent event) {
+	void btnPressChangeSubject(ActionEvent event) {
 		System.out.println("TeacherEditQuestion::btnPressChangeBank");
 		topPanelAp.setDisable(false);
 		botPanelAp.setDisable(true);
-
 	}
 
 	@FXML
