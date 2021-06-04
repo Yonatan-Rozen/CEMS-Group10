d02a1abbb6909cc2574d9511ed9457e6b0616e73
Meitar55e <meitar56789@gmail.com>
2021-06-04 04:10:15 +0300

    directed the "users info" button to the users' info table screen

--- G10_Assignment3-Project/src/gui/client/principle/PrincipleViewInfoController.java
+++ G10_Assignment3-Project/src/gui/client/principle/PrincipleViewInfoController.java
@@ -3,49 +3,56 @@ package gui.client.principle;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import common.CommonMethodsHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 public class PrincipleViewInfoController implements Initializable {
-	
+
 	@FXML
-    private Button sbViewUsersBtn;
+	private Button sbViewUsersBtn;
 
-    @FXML
-    private Button sbViewExamsBtn;
+	@FXML
+	private Button sbViewExamsBtn;
 
-    @FXML
-    private Button sbViewQuestionsBtn;
-    
-    private static Button viewUsersBtn;
-    private static Button viewExamsBtn;
-    private static Button viewQuestionsBtn;
+	@FXML
+	private Button sbViewQuestionsBtn;
+
+	private static Button viewUsersBtn;
+	private static Button viewExamsBtn;
+	private static Button viewQuestionsBtn;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		viewUsersBtn = sbViewUsersBtn;
 		viewExamsBtn = sbViewExamsBtn;
 		viewQuestionsBtn = sbViewQuestionsBtn;
-		
+
 	}
 
-    @FXML
-    void btnPressViewExams(ActionEvent event) {
-    	// TODO get exams data info from the database
-    	System.out.println("PrincipleViewInfo::btnPressViewExams");
-    }
+	@FXML
+	void btnPressViewExams(ActionEvent event) throws Exception {
+		// TODO get exams data info from the database
+		System.out.println("PrincipleViewInfo::btnPressViewExams");
 
-    @FXML
-    void btnPressViewQuestions(ActionEvent event) {
-    	// TODO get questions data info from the database
-    	System.out.println("PrincipleViewInfo::btnPressViewQuestions");
-    }
+	}
 
-    @FXML
-    void btnPressViewUsers(ActionEvent event) {
-    	// TODO get users data info from the database
-    	System.out.println("PrincipleViewInfo::btnPressViewUsers");
-    }
+	@FXML
+	void btnPressViewQuestions(ActionEvent event) {
+		// TODO get questions data info from the database
+		System.out.println("PrincipleViewInfo::btnPressViewQuestions");
+	}
+
+	@FXML
+	void btnPressViewUsers(ActionEvent event) throws Exception {
+		// TODO get users data info from the database
+		System.out.println("PrincipleViewInfo::btnPressViewUsers");
+		// take care of menu bar
+		PrincipleMenuBarController.mainPaneBp.setCenter(CommonMethodsHandler.getInstance().getPane("principle", "PrincipleViewUsersInfoScreen"));
+
+		//ClientUI.mainScene.setRoot(FXMLLoader.load(getClass().getResource("/gui/client/principle/PrincipleViewUsersInfoScreen.fxml")));
+
+	}
 }
