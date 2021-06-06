3019e3bc1b214dad5e635d760e54a64ecce10ec2
Jon <yon969@outlook.co.il>
2021-06-06 12:17:29 +0300

    Added a way to allow only one instance of the server at a time

--- G10_Assignment3-Project/src/server/ServerUI.java
+++ G10_Assignment3-Project/src/server/ServerUI.java
@@ -1,30 +1,49 @@
 package server;
 
+import common.CommonMethodsHandler;
 import gui.server.ServerConsoleController;
+import it.sauronsoftware.junique.AlreadyLockedException;
+import it.sauronsoftware.junique.JUnique;
 import javafx.application.Application;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class ServerUI extends Application{
 	
 	public final static int DEFAULT_PORT = 5555;
 	public static ServerConsoleController serverConsole;
+	private static boolean alreadyRunning;
 	
 	public static void main(String[] args) {
-		launch(args);
+        
+        try {
+            JUnique.acquireLock("Cems-Server");
+            alreadyRunning = false;
+        } catch (AlreadyLockedException e) {
+            alreadyRunning = true; }
+        
+        launch(args); // <-- default JavaFX start
 	}
 
 	@Override
 	public void start(Stage mainStage) throws Exception {
+
+        if (alreadyRunning) {
+        	System.out.println("server instance already exists!");
+        	CommonMethodsHandler.getInstance().getNewAlert(AlertType.INFORMATION, 
+        			"Server already exists","Only one instance of the server is allowed!").showAndWait();
+        	System.exit(1);
+        }
+
 		serverConsole = new ServerConsoleController();
 		serverConsole.start(mainStage);
 	}
 
 	/**
 	 * Starts listening for client connections by using 'EchoServer' which is
-	 * a subclass of 'AbstractServer'.<br>Also starts the connection with the database
+	 * a subclass of 'AbstractServer'.
+	 * 
+	 * <br> Also starts the connection with the database
 	 * @param portNumber the port to listen to connections from
 	 */
 	public static void startServer(int portNumber) {
@@ -34,16 +53,16 @@ public class ServerUI extends Application{
 		try {
 			sv.listen();
 		} catch(Exception ex) {
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.initStyle(StageStyle.UTILITY);
-			alert.setTitle("Server Error");
-			alert.setHeaderText("Could not listent for clients!");
-			alert.setContentText("Make sure there isn't an open server instance on the same port!");
-			alert.showAndWait();
+			try { DBconnector.getInstance().resetUserConnections();
+			}catch(Exception e) { System.out.println("connection isn't working!"); }
+			
+			CommonMethodsHandler.getInstance().getNewAlert(AlertType.ERROR,"Server Error", "Could not listent for clients!",
+					"Make sure there isn't an open server instance on the same port!" ).showAndWait();
 			serverConsole.setDisable(false);
 			return;
 		}
 		
 		DBconnector.getInstance().connectToDB();
+		DBconnector.getInstance().resetUserConnections();
 	}
 }
