a38d222192398b67ca1a3e8918d1b0f1bf0afb4b
Meitar55e <meitar56789@gmail.com>
2021-06-06 21:25:37 +0300

    created an interface of exams for tableView

---
+++ G10_Assignment3-Project/src/logic/exam/IExam.java
@@ -0,0 +1,14 @@
+package logic.exam;
+
+public interface IExam {
+	public String getAllocatedTime();
+
+	public void setAllocatedTime(String allocatedTime);
+
+	public String getExamID();
+	public String getCourseID() ;
+	public String getBankID() ;
+	public String getAuthor() ;
+	public String getType() ;
+
+}
