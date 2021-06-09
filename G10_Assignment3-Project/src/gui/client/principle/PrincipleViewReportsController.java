e17da5e1bcce5ae85620ce4de54b97ca4c98ca1d
Meitar55e <meitar56789@gmail.com>
2021-06-09 21:00:13 +0300

    fixed method btnPressProduceByCourse for when there's no input

--- G10_Assignment3-Project/src/gui/client/principle/PrincipleViewReportsController.java
+++ G10_Assignment3-Project/src/gui/client/principle/PrincipleViewReportsController.java
@@ -16,7 +16,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class PrincipleViewReportsController implements Initializable{
+public class PrincipleViewReportsController implements Initializable {
 
 	// JAVAFX INSTANCES ******************************************************
 	@FXML
@@ -44,11 +44,11 @@ public class PrincipleViewReportsController implements Initializable{
 	private static Button produceByCourseBtn;
 	private static TextField studentIDTf;
 	private static Button produceByStudentBtn;
-	public static String insertedValue="";
+	public static String insertedValue = "";
 
-	// STATIC  INSTANCES **********************************************
-	public static Boolean doesExist=false;
-	public static List<String> list=new ArrayList<>();
+	// STATIC INSTANCES **********************************************
+	public static Boolean doesExist = false;
+	public static List<String> list = new ArrayList<>();
 
 	// CONTROLLER INSTANCES ********************************************
 	public static PrincipleViewReportsController pvrController;
@@ -63,10 +63,10 @@ public class PrincipleViewReportsController implements Initializable{
 		produceByCourseBtn = sbProduceByCourseBtn;
 		studentIDTf = sbstudentIDTf;
 		produceByStudentBtn = sbProduceByStudentBtn;
-		pvrController=new PrincipleViewReportsController();
+		pvrController = new PrincipleViewReportsController();
 		prbtController = new PrincipleReportsByTeacherController();
 		methodsHandler.addTextLimiter(teacherUserNameTf, 9);
-		methodsHandler.addTextLimiter(courseIDTf, 9);//CHECK WAHT ABOUT SUBJECT ID ???????????
+		methodsHandler.addTextLimiter(courseIDTf, 9);// CHECK WAHT ABOUT SUBJECT ID ???????????
 		methodsHandler.addTextLimiter(studentIDTf, 9);
 		PrincipleMenuBarController.menuBarAp.setDisable(false);
 
@@ -77,25 +77,40 @@ public class PrincipleViewReportsController implements Initializable{
 	void btnPressProduceByCourse(ActionEvent event) throws IOException {
 		// TODO show exam reports by course
 		System.out.println("PrincipleViewReports::btnPressProduceByCourse");
-		insertedValue=courseIDTf.getText();
+		insertedValue = courseIDTf.getText();
 		System.out.println(insertedValue);
-		ClientUI.chat.accept(new String[] { "GetTeachers", insertedValue});
-		if(insertedValue.equals("") || insertedValue.equals(null)) {
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "No course ID was inserted.", "Please re-enter.").showAndWait();
+
+		if (insertedValue.equals("") || insertedValue.equals(null)) {
+			System.out.println("INSIDE IF");
+			methodsHandler
+			.getNewAlert(AlertType.ERROR, "Error message", "No course ID was inserted.", "Please re-enter.")
+			.showAndWait();
+		} else if (insertedValue.length() < 4) {
+			methodsHandler.getNewAlert(AlertType.ERROR, "Error message",
+					"The course ID id too short. Make sure you enter the full courseID (including the subject's ID).")
+			.showAndWait();
+		} else if (!checkIfSearchedIDExists(insertedValue, "C")) {
+			methodsHandler
+			.getNewAlert(AlertType.ERROR, "Error message",
+					"This course's ID does not exist in the system's database.", "Please re-enter.")
+			.showAndWait();
 		}
-		else if(insertedValue.length() < 4 ) {
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "The course ID id too short. Make sure you enter the full courseID (including the subject's ID).").showAndWait();
-		}
-		else if(!checkIfSearchedIDExists(insertedValue,"C"))
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "This course's ID does not exist in the system's database.", "Please re-enter.").showAndWait();
-		else if(list.size()==0) {
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "There are no teachers who had an exam done in this course.", "Press OK to return.").showAndWait();
-		}
+		// else
 		else {
-			try {
-				PrincipleMenuBarController.mainPaneBp.setCenter(FXMLLoader.load(getClass().getResource("/gui/client/principle/PrincipleReportsByCourse.fxml")));
-			} catch (IOException e) {
-				e.printStackTrace();
+			ClientUI.chat.accept(new String[] { "GetTeachers", insertedValue });
+			System.out.println("AFTER ACCEPT");
+			if (list.size() == 0) {
+				methodsHandler
+				.getNewAlert(AlertType.ERROR, "Error message",
+						"There are no teachers who had an exam done in this course.", "Press OK to return.")
+				.showAndWait();
+			} else {
+				try {
+					PrincipleMenuBarController.mainPaneBp.setCenter(FXMLLoader
+							.load(getClass().getResource("/gui/client/principle/PrincipleReportsByCourse.fxml")));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
@@ -103,20 +118,25 @@ public class PrincipleViewReportsController implements Initializable{
 	@FXML
 	void btnPressProduceByStudent(ActionEvent event) {
 		System.out.println("PrincipleViewReports::btnPressProduceByStudent");
-		insertedValue=studentIDTf.getText();
-		ClientUI.chat.accept(new String[] { "GetCourses",insertedValue, "S" });
+		insertedValue = studentIDTf.getText();
+		ClientUI.chat.accept(new String[] { "GetCourses", insertedValue, "S" });
 
-		if(insertedValue.equals("") || insertedValue.equals(null)) {
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "No student ID was inserted." ,"Please re-enter.").showAndWait();
-		}
-		else if(!checkIfSearchedIDExists(insertedValue,"S"))
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "This student's ID does not exist in the system's database.","Please re-enter.").showAndWait();
-		else if(list.size()==0) {
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "There are no exams done by this student.","Press OK to return.").showAndWait();
-		}
-		else {
+		if (insertedValue.equals("") || insertedValue.equals(null)) {
+			methodsHandler
+			.getNewAlert(AlertType.ERROR, "Error message", "No student ID was inserted.", "Please re-enter.")
+			.showAndWait();
+		} else if (!checkIfSearchedIDExists(insertedValue, "S"))
+			methodsHandler
+			.getNewAlert(AlertType.ERROR, "Error message",
+					"This student's ID does not exist in the system's database.", "Please re-enter.")
+			.showAndWait();
+		else if (list.size() == 0) {
+			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "There are no exams done by this student.",
+					"Press OK to return.").showAndWait();
+		} else {
 			try {
-				PrincipleMenuBarController.mainPaneBp.setCenter(FXMLLoader.load(getClass().getResource("/gui/client/principle/PrincipleReportsByStudent.fxml")));
+				PrincipleMenuBarController.mainPaneBp.setCenter(FXMLLoader
+						.load(getClass().getResource("/gui/client/principle/PrincipleReportsByStudent.fxml")));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -124,22 +144,29 @@ public class PrincipleViewReportsController implements Initializable{
 	}
 
 	@FXML
-	void btnPressProduceByTeacher(ActionEvent event){
+	void btnPressProduceByTeacher(ActionEvent event) {
 		System.out.println("PrincipleViewReports::btnPressProduceByTeacher");
-		insertedValue=teacherUserNameTf.getText();
-		ClientUI.chat.accept(new String[] { "GetCourses",insertedValue,"P"});
+		insertedValue = teacherUserNameTf.getText();
+		ClientUI.chat.accept(new String[] { "GetCourses", insertedValue, "P" });
 
-		if(insertedValue.equals("") || insertedValue.equals(null)) {
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "No teacher ID was inserted.", "Please re-enter.").showAndWait();
-		}
-		else if(!checkIfSearchedIDExists(insertedValue,"T"))
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "This teacher's ID does not exist in the system's database.","Please re-enter.").showAndWait();
-		else if(list.size()==0) {
-			methodsHandler.getNewAlert(AlertType.ERROR, "Error message", "There are no exams done in any of this teacher's courses.","Press OK to return.").showAndWait();
-		}
-		else {
+		if (insertedValue.equals("") || insertedValue.equals(null)) {
+			methodsHandler
+			.getNewAlert(AlertType.ERROR, "Error message", "No teacher ID was inserted.", "Please re-enter.")
+			.showAndWait();
+		} else if (!checkIfSearchedIDExists(insertedValue, "T"))
+			methodsHandler
+			.getNewAlert(AlertType.ERROR, "Error message",
+					"This teacher's ID does not exist in the system's database.", "Please re-enter.")
+			.showAndWait();
+		else if (list.size() == 0) {
+			methodsHandler
+			.getNewAlert(AlertType.ERROR, "Error message",
+					"There are no exams done in any of this teacher's courses.", "Press OK to return.")
+			.showAndWait();
+		} else {
 			try {
-				PrincipleMenuBarController.mainPaneBp.setCenter(FXMLLoader.load(getClass().getResource("/gui/client/principle/PrincipleReportsByTeacher.fxml")));
+				PrincipleMenuBarController.mainPaneBp.setCenter(FXMLLoader
+						.load(getClass().getResource("/gui/client/principle/PrincipleReportsByTeacher.fxml")));
 
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -153,18 +180,20 @@ public class PrincipleViewReportsController implements Initializable{
 	 * @param value
 	 */
 	public void setDoesExit(boolean value) {
-		doesExist=value;
+		doesExist = value;
 	}
 
 	/**
 	 * checks if the value inserted into the textField is legal
+	 *
 	 * @param userName
 	 * @param type
 	 * @return
 	 */
-	public boolean checkIfSearchedIDExists (String userName,String type) {
-		ClientUI.chat.accept(new String[] { "checkIfSearchedIDExists", insertedValue, type});
-		if(doesExist) return true;
+	public boolean checkIfSearchedIDExists(String userName, String type) {
+		ClientUI.chat.accept(new String[] { "checkIfSearchedIDExists", insertedValue, type });
+		if (doesExist)
+			return true;
 		return false;
 	}
 
