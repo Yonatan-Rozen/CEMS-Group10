ecb8a8f531ee254403b16728a90776ece2ad28f8
Jon <yon969@outlook.co.il>
2021-06-10 19:40:14 +0300

    Completed TeacherReports files (fxml + controller)

--- G10_Assignment3-Project/src/gui/client/teacher/TeacherReportsController.java
+++ G10_Assignment3-Project/src/gui/client/teacher/TeacherReportsController.java
@@ -1,11 +1,13 @@
 package gui.client.teacher;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import client.ChatClient;
 import client.ClientUI;
+import common.IntegerStringConverter;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -13,7 +15,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
@@ -22,17 +23,18 @@ import javafx.scene.control.Label;
 import logic.exam.ExamResults;
 
 public class TeacherReportsController implements Initializable {
-
 	public static TeacherReportsController trController;
 
-	@FXML
-	private BarChart<?, ?> sbHistogramBc;
+	// JAVAFX INSTNCES ******************************************************
 
 	@FXML
-	private CategoryAxis sbCategoryAxis;
+	private ChoiceBox<String> sbCourcesCb;
 
 	@FXML
-	private NumberAxis sbNumberAxis;
+	private BarChart<String, Integer> sbHistogramBc;
+
+	@FXML
+	private NumberAxis sbAmountAxisNa;
 
 	@FXML
 	private Label sbExamIDLbl;
@@ -44,131 +46,142 @@ public class TeacherReportsController implements Initializable {
 	private Label sbAverageLbl;
 
 	@FXML
-	private Button sbNextRepBtn;
-
-	@FXML
 	private Button sbPreviousRepBtn;
 
 	@FXML
-	private ChoiceBox<String> sbCourcesCb;
+	private Button sbNextRepBtn;
 
-	@FXML
-	private Button sbShowReportsByCourseBtn;
-
-	private static BarChart<?, ?> histogramBc;
-	private static CategoryAxis categoryAxis;
-	private static NumberAxis numberAxis;
-	private static Label reportsByLbl;
+	// STATIC JAVAFX INSTANCES **********************************************
+	private static ChoiceBox<String> courcesCb;
+	private static BarChart<String, Integer> histogramBc;
 	private static Label examIDLbl;
 	private static Label medianLbl;
 	private static Label averageLbl;
-	private static Button nextRepBtn;
 	private static Button previousRepBtn;
-	private static Button showReportsByCourseBtn;
-	private static ChoiceBox<String> courcesCb;
+	private static Button nextRepBtn;
 
-	public static ObservableList<String> coursesList = FXCollections.observableArrayList("----------");
-	public static List <ExamResults> examResultsList;
-	private int index=0;
-	private static Series series;
+	// STATIC INSTANCES *****************************************************
+	public static ObservableList<String> coursesList = FXCollections.observableArrayList();
+	public static IntegerStringConverter integerStringConverter = new IntegerStringConverter();
+	private static List<ExamResults> examResultsList = new ArrayList<>();
+	private static Series<String, Integer> series;
+	private int index;
 
-	/*public void start() throws IOException {
-		// example **************************************
-		TeacherMenuBarController.mainPaneBp
-				.setCenter(CommonMethodsHandler.getInstance().getPane("teacher", "TeacherReports"));
-		// **********************************************
-	}
-	 */
+	// INITIALIZE METHOD ****************************************************
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		trController = new TeacherReportsController();
 		histogramBc = sbHistogramBc;
-		categoryAxis = sbCategoryAxis;
-		numberAxis = sbNumberAxis;
+		sbAmountAxisNa.setTickLabelFormatter(integerStringConverter);
 		examIDLbl = sbExamIDLbl;
 		medianLbl = sbMedianLbl;
 		averageLbl = sbAverageLbl;
 		nextRepBtn = sbNextRepBtn;
+		nextRepBtn.setDisable(true);
 		previousRepBtn = sbPreviousRepBtn;
 		previousRepBtn.setDisable(true);
 		courcesCb = sbCourcesCb;
-		showReportsByCourseBtn = sbShowReportsByCourseBtn;
-
-		// set "----------" as the first value of the choice box
-		courcesCb.setValue("----------");
-
+		// reset courses list
+		// coursesList.clear();
 		// set the choice box to get it's items from 'coursesList'
 		courcesCb.setItems(coursesList);
-
-		// set up a listener that sets the disable value of
-		// 'showReportsByCourseBtn' according to the selected value
-		courcesCb.getSelectionModel().selectedItemProperty()
-		.addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
-			if (newValue != null) {
-				if (newValue.equals("----------"))
-					showReportsByCourseBtn.setDisable(true);
-				else
-					showReportsByCourseBtn.setDisable(false);
-			}
-		});
-		showReportsByCourseBtn.setDisable(true);
-
-		if (coursesList.size() == 1) // add courses only once
-			ClientUI.chat.accept(new String[] { "GetCourses", ChatClient.user.getUsername(),"T" });
+		choiceBoxEvents();
 	}
 
 	// ACTION METHODS *******************************************************
 
-
+	/**
+	 * Displays the previous exam report under the same course
+	 * 
+	 * @param event
+	 */
 	@FXML
+	@SuppressWarnings("unchecked")
 	void BtnPressPreviousRep(ActionEvent event) {
 		index--;
-		if(index==examResultsList.size()-2)
+		if (index == examResultsList.size() - 2)
 			nextRepBtn.setDisable(false);
-		if(index==0)
+		if (index == 0)
 			previousRepBtn.setDisable(true);
 		histogramBc.getData().removeAll(series);
 		setExamResultData();
 
 	}
 
+	/**
+	 * Displays the next exam report under the same course
+	 * 
+	 * @param event
+	 */
 	@FXML
+	@SuppressWarnings("unchecked")
 	void btnPressNextRep(ActionEvent event) {
+		histogramBc.getData().removeAll(series);
 		index++;
-		if(index==1)
+		if (index == 1)
 			previousRepBtn.setDisable(false);
-		if(index==examResultsList.size()-1)
-			nextRepBtn.setDisable(true);
-		histogramBc.getData().removeAll(series);
-		setExamResultData();
-
-	}
-
-	@FXML
-	void btnPressShowReportsByCourse(ActionEvent event) {
-		histogramBc.getData().removeAll(series);
-		ClientUI.chat.accept(new String[] { "GetExamDetails",courcesCb.getValue(), ChatClient.user.getUsername(),"T" });
-		if(examResultsList.size()==1)
+		if (index == examResultsList.size() - 1)
 			nextRepBtn.setDisable(true);
 		setExamResultData();
 	}
 
-	public void setCoursesCoiseBox(List<String> list) {
-		coursesList.addAll(list);
+	// INTERNAL USE METHODS *************************************************
+
+	/**
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	private void choiceBoxEvents() {
+		courcesCb.getSelectionModel().selectedItemProperty()
+			.addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+				// show the first exam result of the first
+				histogramBc.getData().removeAll(series);
+				examResultsList.clear();
+				ClientUI.chat.accept(new String[] { "GetExamDetails", courcesCb.getValue(),ChatClient.user.getUsername(), "T" });
+				// reset next button
+				if (examResultsList.size() > 1)
+					nextRepBtn.setDisable(false);
+				else nextRepBtn.setDisable(true);
+
+				// reset previous button
+				previousRepBtn.setDisable(true);
+
+				// reset index
+				index = 0;
+
+				setExamResultData();
+			});
 	}
+	
 
-	public void setExamResultsDetails(List<ExamResults> examResultsList) {
-		TeacherReportsController.examResultsList=examResultsList;
-
-	}
-
-	public void setExamResultData() {
-		ExamResults er=examResultsList.get(index);
+	/**
+	 * Set up the barchart with the current exam result details
+	 */
+	private void setExamResultData() {
+		ExamResults er = examResultsList.get(index);
 		examIDLbl.setText("ExamID: #" + er.getExamID());
-		medianLbl.setText("Median: "+er.getMedian());
-		averageLbl.setText("Average: " + er.getAverage());
-		series= er.getGraph();
+		medianLbl.setText("Median: " + String.format("%.2f", er.getMedian()));
+		averageLbl.setText("Average: " + String.format("%.2f", er.getAverage()));
+		series = er.getGraph();
 		histogramBc.getData().add(series);
 	}
 
+	// EXTERNAL USE METHODS *************************************************
+	/**
+	 * Set coursesList, and initializes courcesCb with the first course name in the list
+	 * @param list The list of courses with exam results
+	 */
+	public void setCoursesCoiseBox(List<String> list) {
+		if (coursesList.isEmpty())
+			coursesList.addAll(list);
+		courcesCb.setValue(coursesList.get(0));
+	}
+
+	/**
+	 * Set up exam results list
+	 * @param examResultsList The exam results list
+	 */
+	public void setExamResultsDetails(List<ExamResults> examResults) {
+		TeacherReportsController.examResultsList.addAll(examResults);
+	}
 }
