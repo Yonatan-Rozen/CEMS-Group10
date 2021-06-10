688fe7428cf8108780ed72fe96c0376fa39ff0da
micka <micka@MSI.mshome.net>
2021-06-10 19:24:48 +0300

    corrected the setCode method and removed defaults

--- G10_Assignment3-Project/src/gui/client/student/StudentEnterCodeController.java
+++ G10_Assignment3-Project/src/gui/client/student/StudentEnterCodeController.java
@@ -26,10 +26,10 @@ public class StudentEnterCodeController implements Initializable {
 	// STATIC JAVAFX INSTANCES **********************************************
 	private static TextField codeTf;
 
-	// STATIC  INSTANCES ****************************************************
+	// STATIC INSTANCES ****************************************************
 	private static String code;
 	private static String examType;
-
+	
 	// INITIALIZE METHOD ****************************************************
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -42,54 +42,58 @@ public class StudentEnterCodeController implements Initializable {
 		// go to [Computerized / Manual] exam screen
 		System.out.println("StudentEnterCode::btnPressStartExam");
 
-		setCode("","");
 		System.out.println(code);
-		if (code.equals(codeTf.getText()))
-		{
-			if(examType.equals("computerized"))
-				ClientUI.mainScene.setRoot(FXMLLoader.load(getClass().getResource("/gui/client/student/StudentTakeComputerizedExam.fxml")));
-			//StudentMenuBarController.smbController.mainPaneBp.setCenter(FXMLLoader.load(getClass().getResource("/gui/client/student/StudentTakeComputerizedExam.fxml")));
-			if(examType.equals("manual"))
-				ClientUI.mainScene.setRoot(FXMLLoader.load(getClass().getResource("/gui/client/student/StudentTakeExamManually.fxml")));
+		if (code.equals(codeTf.getText())) {
+			if (examType.equals("computerized"))
+				ClientUI.mainScene.setRoot(FXMLLoader
+						.load(getClass().getResource("/gui/client/student/StudentTakeComputerizedExam.fxml")));
+			// StudentMenuBarController.smbController.mainPaneBp.setCenter(FXMLLoader.load(getClass().getResource("/gui/client/student/StudentTakeComputerizedExam.fxml")));
+			if (examType.equals("manual"))
+				ClientUI.mainScene.setRoot(
+						FXMLLoader.load(getClass().getResource("/gui/client/student/StudentTakeExamManually.fxml")));
 		}
 
 		// if the student didn't insert the correct code
 		else {
 			CommonMethodsHandler.getInstance().getNewAlert(AlertType.INFORMATION, "Code inserting failed",
-					"the code you inserted is wrong","Please try again").showAndWait();
+					"the code you inserted is wrong", "Please try again").showAndWait();
 			codeTf.clear();
 		}
 
-
-		//*******************************************************************
+		// *******************************************************************
 	}
 
 	// EXTERNAL USE METHODS *************************************************
 	/**
 	 * sets code and exam's type of current exam
-	 * @param codeFromTeacher the code of the exam taken
-	 * @param et the type of the exam (manual or computerized)
+	 * 
+	 * @param codeFromTeacher the code of the exam that is being taken
+	 * @param examType        the type of the exam (manual or computerized)
+	 * @param examID		  the ID of the exam that started
 	 *
-	 * TODO get current code and exam-type from teacher to all active students
+	 *                        TODO get current code and exam-type from teacher to
+	 *                        all active students
 	 */
-	public void setCode(String codeFromTeacher,String et) {
-		if(!codeFromTeacher.equals("") && codeFromTeacher!=null) {
-			code=codeFromTeacher;
-			System.out.println("code="+code);
+	public void setCode(String codeFromTeacher, String examType, String examID) {
+		if (!codeFromTeacher.equals("") && codeFromTeacher != null) {
+			code = codeFromTeacher;
+			System.out.println("code=" + code);
+		} else {
+			CommonMethodsHandler.getInstance().getNewAlert(AlertType.INFORMATION, "Code inserting failed",
+					"There was no code inserted by a teacher", "Please try again").showAndWait();
+		}
+		if (!examType.equals("") && examType != null) {
+			examType = examType;
+			System.out.println("examType=" + examType);
+			if(examType.equals("computerized"))
+				StudentTakeComputerizedExamController.stceController.setExamID(examID);
+			else if(examType.equals("manually"))
+				StudentTakeExamManuallyController.stemController.setExamID(examID);
 		}
 		else {
-			code="1111"; //default exam code for now
-			System.out.println("code="+code);
+			CommonMethodsHandler.getInstance().getNewAlert(AlertType.INFORMATION, "Code inserting failed",
+					"There don't seem to be any details of a running exam").showAndWait();
 		}
-		if(!et.equals("") && et!=null) {
-			examType=et;
-			System.out.println("code="+code);
-		}
-		else {
-			examType="computerized"; //default exam type for now
-			System.out.println("type="+examType);
-		}
-
 	}
 
 }
