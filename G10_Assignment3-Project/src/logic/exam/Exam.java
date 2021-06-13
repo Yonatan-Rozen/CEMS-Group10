15e9666d4e1dd29204829f4567e4d427e90e1206
Meitar55e <meitar56789@gmail.com>
2021-06-14 01:44:44 +0300

    added field + (getters +setters)

--- G10_Assignment3-Project/src/logic/exam/Exam.java
+++ G10_Assignment3-Project/src/logic/exam/Exam.java
@@ -10,6 +10,11 @@ public class Exam implements Serializable,IExam{
 	private String courseID;
 	private String allocatedTime;
 	private String type;
+	private String numtotalStudents;
+	private String numSubmitted_1; // submitted student
+	private String numSubmitted_0; // non submitted students
+	private String duration;
+	private String date;
 
 	public Exam(String examID, String bankID, String courseID, String allocatedTime, String author,String type) {
 		this.examID = examID;
@@ -20,6 +25,17 @@ public class Exam implements Serializable,IExam{
 		this.allocatedTime = allocatedTime;
 
 	}
+
+	@Override
+	public String getDuration() {
+		return duration;
+	}
+
+	@Override
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
 
 	@Override
 	public String getAllocatedTime() {
@@ -56,10 +72,50 @@ public class Exam implements Serializable,IExam{
 		return type;
 	}
 
+	@Override
+	public String getNumtotalStudents() {
+		return numtotalStudents;
+	}
 
+	@Override
+	public void setNumtotalStudents(String numtotalStudents) {
+		this.numtotalStudents = numtotalStudents;
+	}
+
+	@Override
+	public String getNumSubmitted_1() {
+		return numSubmitted_1;
+	}
+
+	@Override
+	public void setNumSubmitted_1(String numSubmitted_1) {
+		this.numSubmitted_1 = numSubmitted_1;
+	}
+
+	@Override
+	public String getNumSubmitted_0() {
+		return numSubmitted_0;
+	}
+
+	@Override
+	public void setNumSubmitted_0(String numSubmitted_0) {
+		this.numSubmitted_0 = numSubmitted_0;
+	}
+
+	@Override
+	public String getDate() {
+		// TODO Auto-generated method stub
+		return date;
+	}
+
+	@Override
+	public void setDate(String date) {
+		// TODO Auto-generated method stub
+		this.date = date;
+	}
 
 	@Override
 	public String toString() {
-		return String.format("%s %s %s %s %s %s ",examID,bankID,courseID,allocatedTime,author,type);
+		return String.format("examID=%s\n bankID=%s\n courseID=%s\n allocatedTime=%s\n author=%s\n type=%s\n numtotalStudents=%s\n numSubmitted_1 =%s\n numSubmitted_0 =%s\n ",examID,bankID,courseID,allocatedTime,author,type,date,duration,numtotalStudents,numSubmitted_1,numSubmitted_0);
 	}
 }
