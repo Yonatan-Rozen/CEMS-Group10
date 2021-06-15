82fa3aa81d4b8fc723caa7c6d2ba2ae1a0dd2796
Meitar55e <meitar56789@gmail.com>
2021-06-15 22:13:07 +0300

    added examiningTeacherID + call for Inc when student enters exam

--- G10_Assignment3-Project/src/gui/client/student/StudentEnterCodeController.java
+++ G10_Assignment3-Project/src/gui/client/student/StudentEnterCodeController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import client.ClientUI;
 import common.CommonMethodsHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -28,9 +29,10 @@ public class StudentEnterCodeController implements Initializable {
 	// STATIC INSTANCES ****************************************************
 	private static String code;
 	private static String examType;
+	private static String examiningTeacherID;
 
 	// CONTROLLER INSTANCES ****************************************************
-	protected static StudentEnterCodeController secController=new StudentEnterCodeController();
+	public static StudentEnterCodeController secController=new StudentEnterCodeController();
 
 	// INITIALIZE METHOD ****************************************************
 	@Override
@@ -44,8 +46,6 @@ public class StudentEnterCodeController implements Initializable {
 		// go to [Computerized / Manual] exam screen
 		System.out.println("StudentEnterCode::btnPressStartExam");
 
-		System.out.println(code);
-
 		if (code.equals(codeTf.getText())) {
 			if (examType.equals("C"))
 				//ClientUI.mainScene.setRoot(FXMLLoader
@@ -56,6 +56,9 @@ public class StudentEnterCodeController implements Initializable {
 				//						FXMLLoader.load(getClass().getResource("/gui/client/student/StudentTakeExamManually.fxml")));
 				StudentMenuBarController.smbController.mainPaneBp.setCenter(FXMLLoader.load(getClass().getResource("/gui/client/student/StudentTakeExamManually.fxml")));
 			StudentMenuBarController.smbController.menuBarContainerAp.setDisable(true);
+
+			//TeacherStartExamController.tseController.studentsInExam++;
+			ClientUI.chat.accept(new String[] {"SendMessageIncNumStudentsInExam",examiningTeacherID}); // TODO (increments the amount of students that are in the running exam)
 		}
 
 		// if the student didn't insert the correct code
@@ -79,14 +82,17 @@ public class StudentEnterCodeController implements Initializable {
 	 *                        TODO get current code and exam-type from teacher to
 	 *                        all active students
 	 */
-	public void setReadyExam(String codeFromTeacher, String examTypeFromTeacher, String examID) {
-		System.out.println("codeFromTeacher = "+codeFromTeacher+"\nexamTypeFromTeacher = "+examTypeFromTeacher+"\nexamID = "+examID);
+	public void setReadyExam(String codeFromTeacher, String examTypeFromTeacher, String examID, String examiningTeacherID) {
+		System.out.println("GOT TO SETREADYEXAM of student ENTER CODE-------------------------->");
+		if(examID==null) System.out.println("GOT TO SETREADYEXAM of student ENTER CODE + null-------------------------->");
 
+		//System.out.println("codeFromTeacher = "+codeFromTeacher+"\nexamTypeFromTeacher = "+examTypeFromTeacher+"\nexamID = "+examID);
+		this.examiningTeacherID=examiningTeacherID;
 		code = codeFromTeacher;
-		System.out.println("code=" + code);
+		//	System.out.println("code=" + code);
 
 		examType = examTypeFromTeacher;
-		System.out.println("examType=" + examType);
+		//System.out.println("examType=" + examType);
 		if(examType.equals("C"))
 			StudentTakeComputerizedExamController.stceController.setExamID(examID);
 		else if(examType.equals("M"))
